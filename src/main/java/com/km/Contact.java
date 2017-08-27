fe351082a38aa579504e5bd620709bcfff02aa38
Krishna Madhur Philkhana <kmkrishna1madhur@gmail.com>
2017-08-26 23:45:10 -0500

    Update Contact.java

--- src/main/java/com/km/Contact.java
+++ src/main/java/com/km/Contact.java
@@ -44,22 +44,10 @@ public class Contact extends HttpServlet {
 	String from = "krishna1madhur@gmail.com";
 	messageBody = "Sender's EMail address: "+email+"\n" + messageBody;
 	
-	final String username = "krishna4madhur";
-	final String password = "kmwebapp";
+	final String username = "**********";
+	final String password = "*******";
 
 	Properties props = new Properties();
-//	props.put("mail.smtp.auth", "true");
-//	props.put("mail.smtp.starttls.enable", "true");
-//	props.put("mail.smtp.host", "smtp.gmail.com");
-//	props.put("mail.smtp.port", "587");
-//	
-	
-//	Session session = Session.getInstance(props,
-//			  new javax.mail.Authenticator() {
-//				protected PasswordAuthentication getPasswordAuthentication() {
-//					return new PasswordAuthentication(username, password);
-//				}
-//			  });
 	
 	Session session = Session.getDefaultInstance(props, null);
 	
